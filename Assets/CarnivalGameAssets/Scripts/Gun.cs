960832561afeff5ce1dd48c1f930dd9ded1e1876
NuIIReference <56423538+NuIIReference@users.noreply.github.com>
2021-08-10 16:42:21 -0700

    Balloon games
    Added a game to inflate the balloons and pop the balloons with a gun

--- Assets/CarnivalGameAssets/Scripts/Gun.cs
+++ Assets/CarnivalGameAssets/Scripts/Gun.cs
@@ -6,29 +6,95 @@ using UnityEngine.InputSystem;
 public class Gun : MonoBehaviour
 {
     [SerializeField]
-    Transform gunTip;
+    bool holdToFire = true;
+
+    [SerializeField]
+    [Range(.001f, .01f)]
+    float inflateSpeed = 0.01f;
+
+    [SerializeField]
+    float fireRate = .5f;
+
+    PlayerInputActions inputActions;
+
+    Transform ballonTransform;
+
+    bool targetHit = false;
 
     private void Awake()
     {
-        PlayerInputActions inputActions = new PlayerInputActions();
+        inputActions = new PlayerInputActions();
         inputActions.Player.Enable();
-        inputActions.Player.Fire.performed += Fire;
+
+        if (!holdToFire)
+        {
+            inputActions.Player.Fire.performed += Fire;
+        }
+        else
+        {
+            inputActions.Player.FireHold.started += StartFiring;
+            inputActions.Player.FireHold.canceled += StopFiring;
+        }
     }
 
+    private void Update()
+    {
+        if (targetHit)
+        {
+            ballonTransform.localScale = Vector3.Lerp(ballonTransform.localScale, ballonTransform.localScale + Vector3.one, inflateSpeed);
+        }        
+    }
+
+    //Used to fire the gun on mouse click
     public void Fire(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Debug.Log("Shoot - " + context.phase);
-
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f))
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 1000f))
             {
-                if (hit.collider.CompareTag("Target"))
+                Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.forward * 1000f, Color.red);
+                if (hit.collider.CompareTag("Balloon"))
                 {
                     Debug.Log("Hit Target");
-                    hit.transform.GetChild(0).localScale += Vector3.one * .5f;
+                    hit.collider.gameObject.SetActive(false);
                 }
             }
         }
+    }
+
+    //Used to fire the gun continuously
+    void HoldToFire()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 1000f))
+        {
+            Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.forward * 1000f, Color.red);
+            if (hit.collider.CompareTag("Target"))
+            {
+                ballonTransform = hit.transform.GetChild(0);
+                targetHit = true;
+            }
+            else
+            {
+                targetHit = false;
+            }
+        }
+    }
+
+    void StartFiring(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            InvokeRepeating("HoldToFire", 0f, fireRate);
+        }        
+    }
+
+    void StopFiring(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            Debug.Log("Stop Firing");
+            CancelInvoke();
+            targetHit = false;
+        }        
     }
 }
